9716c2e2b991966e775165cd1ede4d72b99c5a40
student <otari.bakashvili.1@btu.edu.ge>
2023-04-19 16:20:50 +0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/fragment/ThirdFragment.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication.fragment
+
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+class ThirdFragmentFragment : Fragment(R.layout.fragment_third) {
+}
